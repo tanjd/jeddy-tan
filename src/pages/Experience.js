466318a3ce1fa1f3fd56a3a08acf49f18acc81b9
Jeddy Tan <tanjd@hotmail.com>
2022-11-27 16:23:41 +0800

    added timeline and change css colours

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,7 +1,40 @@
-// import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import SchoolIcon from '@mui/icons-material/School';
+import WorkIcon from '@mui/icons-material/Work';
 
 function Experience() {
-  return <div>Experience</div>;
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#192428">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2022 - present"
+          iconStyle={{ background: '#d8ab4e', color: '#192428' }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Graduate/Junior Software Engineer - ExpressVPN
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">Singapore, SG</h4>
+          <p>
+            Helped the team launch 2 major features by working both in the front end and back end.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2018 - 2022"
+          iconStyle={{ background: '#192428', color: '#fff' }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Singapore Management University - School of Computing and Information System
+          </h3>
+          <p>Bachelors of science</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Experience;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,10 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import '../styles/Home.css';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import SchoolIcon from '@mui/icons-material/School';
+import WorkIcon from '@mui/icons-material/Work';
 
 function Home() {
   return (
@@ -9,28 +13,40 @@ function Home() {
       <div className="about">
         <h2> Hi, My name is Jeddy</h2>
         <div className="prompt">
-          <p>Junior Software Engineer @ ExpressVPN</p>
+          <p>Description about self</p>
           <GitHubIcon />
           <EmailIcon />
           <LinkedInIcon />
         </div>
       </div>
-      <div className="skills">
-        <h1>Skills</h1>
-        <ol className="list">
-          <li className="item">
-            <h2>Front-End</h2>
-            <span></span>
-          </li>
-          <li className="item">
-            <h2>Back-End</h2>
-            <span></span>
-          </li>
-          <li className="item">
-            <h2>Devops</h2>
-            <span></span>
-          </li>
-        </ol>
+      <div className="experience">
+        <VerticalTimeline lineColor="#192428">
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2022 - present"
+            iconStyle={{ background: '#d8ab4e', color: '#192428' }}
+            icon={<WorkIcon />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Graduate/Junior Software Engineer - ExpressVPN
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">Singapore, SG</h4>
+            <p>
+              Helped the team launch 2 major features by working both in the front end and back end.
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="2018 - 2022"
+            iconStyle={{ background: '#192428', color: '#fff' }}
+            icon={<SchoolIcon />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Singapore Management University - School of Computing and Information System
+            </h3>
+            <p>Bachelors of science</p>
+          </VerticalTimelineElement>
+        </VerticalTimeline>
       </div>
     </div>
   );
