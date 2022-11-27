6a0a3d20a3d170d85f9dbbba8b1ffc83d74aaed8
Jeddy Tan <tanjd@hotmail.com>
2022-11-27 14:18:11 +0800

    added css for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,39 @@
-// import React from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import '../styles/Home.css';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="about">
+        <h2> Hi, My name is Jeddy</h2>
+        <div className="prompt">
+          <p>Junior Software Engineer @ ExpressVPN</p>
+          <GitHubIcon />
+          <EmailIcon />
+          <LinkedInIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2>Front-End</h2>
+            <span></span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span></span>
+          </li>
+          <li className="item">
+            <h2>Devops</h2>
+            <span></span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
