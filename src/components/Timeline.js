4d25343bae9d0fbd844401dc87346941357235be
Jeddy Tan <tanjd@hotmail.com>
2022-11-28 00:45:40 +0800

    refractor data into a data.json

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,90 +2,38 @@ import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeli
 import 'react-vertical-timeline-component/style.min.css';
 import SchoolIcon from '@mui/icons-material/School';
 import WorkIcon from '@mui/icons-material/Work';
+import Experiences from '../data.json';
 
 function Timeline() {
   return (
     <div className="experience">
       <VerticalTimeline lineColor="#192428">
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="Jul 2022 - present"
-          iconStyle={{ background: '#d8ab4e', color: '#192428' }}
-          icon={<WorkIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Graduate/Junior Software Engineer - ExpressVPN
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">Singapore, SG</h4>
-          <p>
-            <b>Web Applications Platform (WAP):</b>
-            <li>
-              Designed and built the CICD pipeline for a monorepo using github actions and NX tool.
-              The monorepo&apos;s CICD pipeline is able to continuously integrate, deploy and
-              deliver different types of applications (e.g, react and fastapi) that are affected by
-              code change concurrently.
-            </li>
-          </p>
-          <p>
-            <b>Internal Apps & Automation (IAA):</b>
-            <li>
-              Build tools to automate processes and workflows for various teams.{' '}
-              <i>
-                E.g., Automating the employee offloading process that helps remove offloading
-                employee&apos;s access to various applications
-              </i>
-            </li>
-          </p>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="May 2021 - Aug 2021"
-          iconStyle={{ background: '#192428', color: '#fff' }}
-          icon={<WorkIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Summer Intern - Cybersecurity Agency Of Singapore (CSA)
-          </h3>
-          <p>
-            <b>Engineering Center:</b>
-            <li>
-              Worked on a prototype that performs automated malware analysis & testing on android
-              applications
-            </li>
-          </p>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="May 2020 - Aug 2020"
-          iconStyle={{ background: '#192428', color: '#fff' }}
-          icon={<WorkIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Summer Intern - Government Technology Agency (Govtech)
-          </h3>
-          <p>
-            <b>Secure Software Development & Audit:</b>
-            <li>
-              Performed static code analysis on open-source libraries used in sensitive projects to
-              identify potential vulnerabilities
-            </li>
-          </p>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="Aug 2018 - May 2022"
-          iconStyle={{ background: '#192428', color: '#fff' }}
-          icon={<SchoolIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">Singapore Management University</h3>
-          <p>
-            <b>Bachelors of science (Information Systems & Management)</b> - Major in Software
-            Development & Cybersecurity
-          </p>
-        </VerticalTimelineElement>
+        {Experiences.experiences.map((item, id) => (
+          <VerticalTimelineElement
+            key={id}
+            className={
+              item.type == 'work'
+                ? 'vertical-timeline-element--work'
+                : 'vertical-timeline-element--education'
+            }
+            date={item.date}
+            iconStyle={{
+              background: item.isActive ? '#d8ab4e' : '#192428',
+              color: item.isActive ? '#192428' : '#fff',
+            }}
+            icon={item.type == 'work' ? <WorkIcon /> : <SchoolIcon />}
+          >
+            <h3 className="vertical-timeline-element-title">{item.title}</h3>
+            {item.subtitle && (
+              <h4 className="vertical-timeline-element-subtitle">{item.subtitle}</h4>
+            )}
+            {item.descriptions.map(desc => (
+              <p key={desc}>
+                <div dangerouslySetInnerHTML={{ __html: desc }} />
+              </p>
+            ))}
+          </VerticalTimelineElement>
+        ))}
       </VerticalTimeline>
     </div>
   );
